04adcfa6b175cdda8cb08cb349742b27c5980b31
ShahrulMustaqiim <mustaqimshahrul04@gmail.com>
2026-06-01 10:09:00 +0800

    Updated Navbar display on all devices

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
     <router-link class="navbar-brand" to="/">MalaysiaFoodHub</router-link>
 
     <!-- Mobile Toggle Button -->
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
